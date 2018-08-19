80c81dd2e247453791b3f49a51bc0952394c2ce7
andandandand <antonio.rueda.toicen@gmail.com>
2018-08-18 21:44:46 -0400

    added rest button

--- test_delete_vertices.R
+++ test_delete_vertices.R
@@ -1,14 +1,17 @@
 library("igraph")
 
-g <- make_star(10, mode="undirected") %>% set_vertex_attr("name", value = 1:10)
+#g <- make_star(10, mode="undirected") %>% set_vertex_attr("name", value = 1:10)
 
-plot(g)
+#plot(g)
 
 #reduced_g <- delete_vertices(g, lapply(1:8, toString))
 
 #reduced_g <- delete_vertices(g, 1:8)
 
 #check previous stack overflow question to solve this issue
-reduced_g <- delete_vertices(g, 1)
+#reduced_g <- delete_vertices(g, 1)
 
-plot(reduced_g)
+#plot(reduced_g)
+
+g <- erdos.renyi.game(14, 1/10)
+plot(g)
